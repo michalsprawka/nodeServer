2e067d18490f733a056eadefa65f5eac86a8094a
michalsprawka <mispraw@wp.pl>
2020-10-03 06:22:53 +0200

    api post1

--- routes/api/words.js
+++ routes/api/words.js
@@ -10,4 +10,31 @@ router.get('/all', (req, res)=> {
     })
 })
 
+router.post('/add', (req, res) => {
+    console.log(req.body);
+    let newWord = new Word({
+        itaWord: req.body.itaword,
+        polWord: req.body.polword
+    })
+    Word.findOne({itaWord: newWord.itaWord})
+    .exec()
+    .then(found => {
+        if(!found){
+            console.log("FOUND", found)
+            newWord.save()
+            .then(result => {
+                res.json(result);
+            }
+            )
+            .catch(error => {
+             if(error) res.json(error);
+             })
+        } else {
+            console.log("istnieje", found);
+            res.json({message: "Word exist"});
+
+        }// end of if res
+    })  
+})
+
 module.exports = router;
