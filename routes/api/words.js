4d0affc256b7706c2dfdd80548ee9a6c676e8fe8
michalsprawka <mispraw@wp.pl>
2020-10-03 06:30:24 +0200

    api get random

--- routes/api/words.js
+++ routes/api/words.js
@@ -10,6 +10,22 @@ router.get('/all', (req, res)=> {
     })
 })
 
+router.get('/randomword/', (req, res) => {
+    Word.count().exec(function (err, count) {
+
+        // Get a random entry
+        var random = Math.floor(Math.random() * count)
+      
+        // Again query all users but only fetch one offset by our random #
+        Word.findOne().skip(random).exec(
+          function (err, result) {
+            // Tada! random user
+            console.log(result) 
+            res.json(result)   
+          })
+      })
+})
+
 router.post('/add', (req, res) => {
     console.log(req.body);
     let newWord = new Word({
