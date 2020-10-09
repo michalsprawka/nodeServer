cf6cbd74cf4fdc0460646d05aa5884e788ba6ae6
michalsprawka <mispraw@wp.pl>
2020-10-09 03:50:21 +0200

    clear index js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const port = 3000, express = require("express"), app=express();
-const router = express.Router();
-const homeController = require('./controllers/homeController');
+//const router = express.Router();
+//const homeController = require('./controllers/homeController');
 const layouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const Word = require("./models/word");
@@ -9,12 +9,12 @@ const flash = require('connect-flash'); //+
 const session = require('express-session'); //+
 const passport = require("passport"); //+
 const dotenv = require('dotenv');
+
 dotenv.config();
-console.log("FROM ENV",process.env.DBUSER, process.env.DBPASS);
+//console.log("FROM ENV",process.env.DBUSER, process.env.DBPASS);
 const mongoURI = `mongodb://${process.env.DBUSER}:${process.env.DBPASS}@ds129352.mlab.com:29352/wordsdb`;
-mongoose.Promise = global.Promise;
-//test
-//passport config:
+//mongoose.Promise = global.Promise;
+
 require('./config/passport')(passport) //+
 
 mongoose.connect(mongoURI);
@@ -23,19 +23,6 @@ db.once("open", ()=>{
 	console.log("Succesfully connected to mongoDB using mongoose");
 });
 
-// Word.create(
-// 	{itaWord: "rosso", polWord: "czerwony"},
-// 	function(error, savedDocument) {
-// 		if(error) console.log(error);
-// 		console.log(savedDocument);
-// 	}
-// )
-
-// Word.findOne({
-// 	polWord: "zielony"
-// },(err, data) => {
-// 	if(data) console.log(data);
-// });
 
 app.use(
 	express.urlencoded({
@@ -44,11 +31,12 @@ app.use(
   );
 
 
-  app.use(session({ //+
+app.use(session({ //+
     secret : 'secret',
     resave : true,
     saveUninitialized : true
 }));
+
 app.use(passport.initialize()); //+
 app.use(passport.session()); //+
 app.use(flash()); //+
@@ -59,37 +47,17 @@ app.use((req,res,next)=> { //+
     next();
     })
 app.use(function (req, res, next) {
-		//console.log(req.session);
-		// if(req.session.passport && req.session.passport.user){
-		// res.locals.loggedIn = req.user;
-		// } else{
-		// res.locals.loggedIn = null;
-		// }
 		res.locals.loggedIn = req.isAuthenticated()
 		res.locals.user = req.user
 		next()
 		})
-app.use(express.json());
 
+app.use(express.json());
 app.use(layouts);
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-// app.get('/items/:vegetable', homeController.sendReqParam);
-// app.get('/name/:myName', homeController.respondWithName);
 
-
-// app.get("/words", wordsController.getAllWords, (req, res,next) => {
-// 	//console.log(req.data);
-// 	res.render("words", {words: req.data});
-// })
-// app.get("/addform/:ita/:pol/:id", wordsController.getAddWordPAge);
-// app.get("/addform", wordsController.getAddWordPAge);
-// app.post("/word", wordsController.saveWord);
-// app.post("/editword/:id", wordsController.editWord);
-// app.get("/random/:lang", wordsController.getRandomWord);
-// app.get("/wordid/:id/:lang", wordsController.getWordWithId);
-// app.get("/delword/:id", wordsController.deleteWord);
 app.use('/',require('./routes/index')); //+
 app.use('/dict', wordsController)
 app.use('/users',require('./routes/users')); //+
